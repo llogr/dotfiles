c0bc36dcefd7eda200e603da1b5371dff089eded
Gary Lloyd <gareth.lloyd@tessella.com>
2015-03-16 15:06:43 +0000

    Add Windows git-bash prompt configuration

---
+++ windows/git-bash-theme.sh
@@ -0,0 +1,50 @@
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+ORANGE="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
+RESET="\033[m"
+ 
+# Checks if working tree is dirty
+function parse_git_dirty() {
+  local STATUS=''
+  local FLAGS
+  FLAGS=('--porcelain')
+  STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+
+  if [[ -n $STATUS ]]; then
+    echo "$GIT_PROMPT_DIRTY"
+  else
+    echo "$GIT_PROMPT_CLEAN"
+  fi
+}
+
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$(parse_git_dirty)$GIT_PROMPT_PREFIX$(current_branch)$GIT_PROMPT_SUFFIX"
+}
+
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function get_pwd() {
+  print -D $PWD
+}
+
+function spacing() {
+  echo "                                           "
+}
+
+PS1="\[${GREEN}\]\u \[$BLUE\]\w $(spacing)$(git_prompt_info)
+\$ \[$RESET\]"
+
+GIT_PROMPT_PREFIX="[git:"
+GIT_PROMPT_SUFFIX="]$reset_color"
+GIT_PROMPT_DIRTY="\[${ORANGE}\]+"
+GIT_PROMPT_CLEAN="\[${GREEN}\]"
